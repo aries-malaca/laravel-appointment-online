be2bff5af6fd44de58913564c7098a7aac0f7723
Aries <aries@lay-bare.com>
2017-08-29 18:01:23 +0800

    jwt activation

---
+++ public/app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-link :to="{ name: 'Hello' }">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/coins/ethereum">Ethereum</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>

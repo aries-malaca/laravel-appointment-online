ce82a3d7082756b2e7d6249f0dd25ccfc4faa0ae
Aries <aries@lay-bare.com>
2018-01-25 16:50:24 +0800

    a

--- public/app/src/tools/Promotions.vue
+++ public/app/src/tools/Promotions.vue
@@ -27,7 +27,14 @@
                                     :onClick="promotionTable.rowClicked" styleClass="table table-bordered table-hover table-striped" />
                     </div>
                     <div class="tab-pane" id="perks">
-
+                        <button type="button" class="btn green-meadow" @click="showAddPerkModal">New Perk</button>
+                        <br/><br/>
+                        <div class="row">
+                            <div v-for="perk in perks" :class="perk.perk_data.classname" >
+                                <img :src="'../../images/perks/'+perk.perk_picture" alt="" class="img-responsive"
+                                     style="margin-bottom:25px; cursor: pointer;" @click="viewPerk(perk)">
+                            </div>
+                        </div>
                     </div>
                     <div class="tab-pane" id="surveys">
                         <br/><br/>
@@ -63,8 +70,8 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="row" v-if="newPromotion.id != 0">
-                            <div class="col-md-4">
+                        <div class="row">
+                            <div class="col-md-4" v-if="newPromotion.id != 0">
                                 <upload-form :token="token" input_id="promo_file" form_id="promo_form" category="promotion"
                                  :placeholder_image="'images/promotions/'+newPromotion.promo_picture"
                                  :param_url="'promo_id='+newPromotion.id" @emit_host="getPromos" />
@@ -92,6 +99,53 @@
                         <button type="button" class="btn dark btn-outline" data-dismiss="modal">Close</button>
                         <button type="button" v-if="newPromotion.id==0" @click="addPromo($event)" data-loading-text="Saving..." class="btn green">Save</button>
                         <button type="button" v-else @click="updatePromo($event)" data-loading-text="Updating..." class="btn green">Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" id="add-perk-modal" tabindex="-1" role="basic" aria-hidden="true">
+            <div class="modal-dialog modal-md">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                        <h4 class="modal-title" v-if="newPerk.id==0">Add Perk</h4>
+                        <h4 class="modal-title" v-else>Edit Perk</h4>
+                    </div>
+                    <div class="modal-body" v-if="newPerk.perk_data !== undefined">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>Perk Name</label>
+                                    <input type="text" v-model="newPerk.perk_name" class="form-control"/>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>Description</label>
+                                    <textarea class="form-control" v-model="newPerk.perk_description" rows="2"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Grid Type</label>
+                                <select class="form-control" v-model="newPerk.perk_data.classname">
+                                    <option value="col-sm-4">col-sm-4</option>
+                                    <option value="col-sm-3">col-sm-3</option>
+                                </select>
+                            </div>
+                            <div class="col-md-6" v-if="newPerk.id != 0">
+                                <upload-form :token="token" input_id="perk_file" form_id="perk_form" category="promotion"
+                                             :placeholder_image="'images/perks/'+newPerk.perk_picture"
+                                             :param_url="'perk_id='+newPerk.id" @emit_host="getPerks" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn dark btn-outline" data-dismiss="modal">Close</button>
+                        <button type="button" v-if="newPerk.id==0" @click="addPerk($event)" data-loading-text="Saving..." class="btn green">Save</button>
+                        <button type="button" v-else @click="updatePerk($event)" data-loading-text="Updating..." class="btn green">Save</button>
                     </div>
                 </div>
             </div>
@@ -161,6 +215,7 @@
             },
             getPerks:function () {
                 this.getData('/api/promotion/getPerks', 'perks');
+                $("#add-perk-modal").modal("hide");
             },
             getBranches:function () {
                 this.getData('/api/branch/getBranches', 'branches');
@@ -179,24 +234,57 @@
                 $("#summernote_1").summernote({height:200});
                 $("#add-promo-modal").modal("show");
             },
+            showAddPerkModal:function(){
+                this.newPerk = {
+                    id:0,
+                    perk_name:'',
+                    perk_description:'',
+                    perk_picture:'',
+                    perk_order:0,
+                    perk_data:{
+                        classname:'col-sm-4'
+                    }
+                };
+                $("#add-perk-modal").modal("show");
+            },
             viewPromotion:function(promotion){
+                $("#add-promo-modal").modal("show");
                 this.newPromotion = {
                     id:promotion.id,
                     title:promotion.title,
                     type:promotion.type,
-                    description:'',
+                    description:promotion.description,
                     promo_picture:promotion.promo_picture,
                     date_start:moment().format("YYYY-MM-DD"),
                     date_end:moment().format("YYYY-MM-DD"),
                     branches:[0]
                 };
-                $("#summernote_1").summernote({height:200});
-                $("#summernote_1").summernote('code', promotion.description);
+                setTimeout(function(){
+                    $("#summernote_1").summernote({height:200}).summernote('code', promotion.description);
+                },200);
                 try{
                     $("form")[0].reset();
                 }
                 catch(error){}
-                $("#add-promo-modal").modal("show");
+
+            },
+            viewPerk:function(perk){
+                this.newPerk = {
+                    id:perk.id,
+                    perk_name:perk.perk_name,
+                    perk_description:perk.perk_description,
+                    perk_picture:perk.perk_picture,
+                    perk_order:perk.perk_order,
+                    perk_data:{
+                        classname:perk.perk_data.classname
+                    }
+                };
+
+                try{
+                    $("form")[1].reset();
+                }
+                catch(error){}
+                $("#add-perk-modal").modal("show");
             },
             viewSurvey:function(survey){
 
@@ -240,6 +328,36 @@
                     XHRCatcher(error);
                     $btn.button('reset');
                 });
+            },
+            addPerk:function(){
+                let u = this;
+                let $btn = $(event.target);
+                $btn.button('loading');
+
+                this.makeRequest('/api/promotion/addPerk?token=' + this.token, 'post', this.newPerk, function(){
+                    u.getPerks();
+                    toastr.success("Perk added successfully.");
+                    $btn.button('reset');
+                    $("#add-perk-modal").modal('hide');
+                },function(error){
+                    XHRCatcher(error);
+                    $btn.button('reset');
+                });
+            },
+            updatePerk:function(){
+                let u = this;
+                let $btn = $(event.target);
+                $btn.button('loading');
+
+                this.makeRequest('/api/promotion/updatePerk?token=' + this.token, 'patch', this.newPerk, function(){
+                    u.getPerks();
+                    toastr.success("Perk updated successfully.");
+                    $btn.button('reset');
+                    $("#add-perk-modal").modal('hide');
+                },function(error){
+                    XHRCatcher(error);
+                    $btn.button('reset');
+                });
             }
         },
         mounted:function(){
@@ -248,6 +366,7 @@
             this.getPromos();
             this.getPerks();
             this.getBranches();
+            $("#summernote_1").summernote({height:200}).summernote('code', "");
         },
         watch:{
             'newPromotion.type':function(){

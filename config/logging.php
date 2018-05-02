8dcb27af517b2760d3087052645f661a9e88bd8d
Aries <aries@lay-bare.com>
2018-05-02 16:03:58 +0800

    a

--- config/logging.php
+++ config/logging.php
@@ -27,7 +27,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'],
         ],
         'single' => [
             'driver' => 'single',
